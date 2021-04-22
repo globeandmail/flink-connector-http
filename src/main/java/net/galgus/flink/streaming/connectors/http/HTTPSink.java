44f89abd60697f5e7fc62dba2025ff11fa37493c
ashwini <APadhy@globeandmail.com>
2021-04-22 14:25:13 -0400

    SSL verification reverted

--- src/main/java/net/galgus/flink/streaming/connectors/http/HTTPSink.java
+++ src/main/java/net/galgus/flink/streaming/connectors/http/HTTPSink.java
@@ -30,29 +30,7 @@ public class HTTPSink<IN> extends RichSinkFunction<IN> {
             URL url = new URL(httpConnectionConfig.getEndpoint());
 
             long start = System.nanoTime();
-            TrustManager[] trustAllCerts = new TrustManager[] {
-                    new X509TrustManager() {
-                        public java.security.cert.X509Certificate[] getAcceptedIssuers() {
-                            return null;
-                        }
-
-                        public void checkClientTrusted(X509Certificate[] certs, String authType) {  }
-
-                        public void checkServerTrusted(X509Certificate[] certs, String authType) {  }
-                    }
-            };
-
-            SSLContext sc = SSLContext.getInstance("SSL");
-            sc.init(null, trustAllCerts, new java.security.SecureRandom());
-
-            HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
-
-            HttpsURLConnection.setDefaultHostnameVerifier(new HostnameVerifier() {
-                @Override
-                public boolean verify(String s, SSLSession sslSession) {
-                    return true;
-                }
-            });
+            
             HttpURLConnection conn = httpConnectionConfig.isHttpsEnabled() ? (HttpsURLConnection) url.openConnection() : (HttpURLConnection) url.openConnection();
 
             conn.setDoOutput(true);
